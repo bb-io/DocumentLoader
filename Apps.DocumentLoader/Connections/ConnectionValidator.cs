2e7d0bbbf4824e562000338de94a3719f153c60d
bzveriok <bohdan.zverok@blackbird.io>
2023-10-26 14:20:07 +0300

    artifactory preparations

---
+++ Apps.DocumentLoader/Connections/ConnectionValidator.cs
@@ -0,0 +1,16 @@
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.DocumentLoader.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        return new(Task.FromResult(new ConnectionValidationResponse()
+        {
+            IsValid = true
+        }));
+    }
+}
