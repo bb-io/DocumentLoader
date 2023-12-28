c0278b3c0560ac117028892586b3b16af31e68e7
bZverok <137277669+bZverok@users.noreply.github.com>
2023-12-28 13:05:23 +0200

    New files (#6)
    * added new files
    
    * fixed csproj
    
    * Update Apps.DocumentLoader.csproj

--- Apps.DocumentLoader/Actions.cs
+++ Apps.DocumentLoader/Actions.cs
@@ -6,39 +6,48 @@ using Apps.DocumentLoader.Models.Requests;
 using Apps.DocumentLoader.Models.Responses;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Spreadsheet;
-using File = Blackbird.Applications.Sdk.Common.Files.File;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using Blackbird.Applications.Sdk.Utils.Extensions.Files;
 
 namespace Apps.DocumentLoader;
 
 [ActionList]
 public class Actions
 {
+    private readonly IFileManagementClient _fileManagementClient;
+
+    public Actions(IFileManagementClient fileManagementClient)
+    {
+        _fileManagementClient = fileManagementClient;
+    }
+
     [Action("Load document", Description = "Load document's text. Document must be in docx/doc, pdf or txt format.")]
     public async Task<LoadDocumentResponse> LoadDocument([ActionParameter] LoadDocumentRequest request)
     {
-        var text = await DocumentReader.ReadDocument(request.File.Bytes, request.File.Name);
-        return new LoadDocumentResponse { Text = text };
+        var file = await _fileManagementClient.DownloadAsync(request.File);
+        var text = await DocumentReader.ReadDocument(await file.GetByteData(), request.File.Name);
+
+        return new() { Text = text };
     }
 
     [Action("Split document into chunks", Description = "Split loaded document into chunks.")]
     public SplitDocumentResponse SplitDocument([ActionParameter] SplitDocumentRequest request)
     {
-        var textSplitter = new TextSplitter(maximumChunkSize: request.MaximumChunkSize ?? 4000, 
+        var textSplitter = new TextSplitter(maximumChunkSize: request.MaximumChunkSize ?? 4000,
             maximumChunkOverlap: request.MaximumChunkOverlap ?? 200);
         var chunks = textSplitter.SplitText(request.Document);
         return new SplitDocumentResponse { Chunks = chunks };
     }
 
     [Action("Convert text to txt document", Description = "Convert text to txt document.")]
-    public ConvertTextToDocumentResponse ConvertTextToTxtDocument([ActionParameter] ConvertTextToDocumentRequest request)
+    public async Task<ConvertTextToDocumentResponse> ConvertTextToTxtDocument(
+        [ActionParameter] ConvertTextToDocumentRequest request)
     {
         const string txtExtension = ".txt";
-        
+
         var bytes = Encoding.UTF8.GetBytes(request.Text);
         var filename = request.Filename;
-        
+
         if (!Path.HasExtension(filename))
             filename += txtExtension;
         else
@@ -49,56 +58,48 @@ public class Actions
                                     "don't specify extension at all.");
         }
 
+        var file = await _fileManagementClient.UploadAsync(new MemoryStream(bytes), MediaTypeNames.Application.Octet,
+            filename);
         return new ConvertTextToDocumentResponse
         {
-            File = new File(bytes)
-            {
-                Name = filename,
-                ContentType = MediaTypeNames.Application.Octet
-            }
+            File = file
         };
     }
 
     [Action("Change XML file property", Description = "Change XML file property")]
     public async Task<ConvertTextToDocumentResponse> ChangeXML([ActionParameter] ChangeXMLRequest request)
     {
-        using(MemoryStream streamIn = new MemoryStream(request.File.Bytes))
-        {
-            XDocument doc = XDocument.Load(streamIn);
-            var items = doc.Root.Descendants(request.Property);
+        await using var streamIn = await _fileManagementClient.DownloadAsync(request.File);
+        var doc = XDocument.Load(streamIn);
+        var items = doc.Root.Descendants(request.Property);
 
-            foreach (var itemElement in items)
-            {
-                itemElement.Value = request.Value;
-            }
-            using (MemoryStream streamOut = new MemoryStream())
-            {
-                XmlWriterSettings settings = new XmlWriterSettings();
-                settings.OmitXmlDeclaration = true;
-                settings.Indent = true;
-                XmlWriter writer = XmlWriter.Create(streamOut, settings);
-                doc.Save(writer);
-                writer.Flush();
-                return new ConvertTextToDocumentResponse { 
-                    File = new File(streamOut.ToArray())
-                    {
-                        ContentType = request.File.ContentType,
-                        Name = request.File.Name
-                    }
-                };
-            }
-        } 
+        foreach (var itemElement in items)
+            itemElement.Value = request.Value;
+
+        await using var streamOut = new MemoryStream();
+        var settings = new XmlWriterSettings();
+        settings.OmitXmlDeclaration = true;
+        settings.Indent = true;
+        var writer = XmlWriter.Create(streamOut, settings);
+        doc.Save(writer);
+        await writer.FlushAsync();
+
+        var resultFile =
+            await _fileManagementClient.UploadAsync(streamOut, request.File.ContentType, request.File.Name);
+        return new ConvertTextToDocumentResponse
+        {
+            File = resultFile
+        };
     }
 
     [Action("Get XML file property", Description = "Get XML file property")]
     public async Task<GetXMLPropertyResponse> GetXMLProperty([ActionParameter] GetXMLPropertyRequest request)
     {
-        using (MemoryStream streamIn = new MemoryStream(request.File.Bytes))
-        {
-            XDocument doc = XDocument.Load(streamIn);
-            var items = doc.Root.Descendants(request.Property);
-            return new GetXMLPropertyResponse() { Value = items.FirstOrDefault().Value };
-        }
+        await using var streamIn = await _fileManagementClient.DownloadAsync(request.File);
+
+        var doc = XDocument.Load(streamIn);
+        var items = doc.Root.Descendants(request.Property);
+        return new() { Value = items.First().Value };
     }
 
     [Action("Bump version string", Description = "Bump version string")]
